7e90f5d3cb57a8c02e6766749fb89c669f305575
Kems <rufai@hotmail.co.uk>
2019-11-28 19:13:34 +0000

    adding port

--- server.js
+++ server.js
@@ -32,8 +32,8 @@ app.get('/profile/:id', (req, res) => { image.handleProfileGet(req, res, db)});
 app.put('/image', (req, res) => { image.handleImage(req, res, db)});
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)});
 
-app.listen(3000, () => {
-	console.log('app is running on pot 3000');
+app.listen(process.env.PORT || 3000, () => {
+	console.log('app is running on ${process.env.PORT}');
 })
 
 /*
